318da719eba6202809f10d03c573f79204b61822
koush <koushamoeini@gmail.com>
2025-11-16 15:56:54 +0330

    feat(client): Add Java module configuration
    - Configure JavaFX controls and FXML modules
    
    - Export client packages for JavaFX reflection
    
    - Set up modular application structure

---
+++ clientCurveFever/src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module com.example.clientap6 {
+    requires javafx.controls;
+    requires javafx.fxml;
+            
+                            
+    opens com.example.clientap6 to javafx.fxml;
+    exports com.example.clientap6;
+    exports com.example.clientap6.controllers;
+    opens com.example.clientap6.controllers to javafx.fxml;
+}
